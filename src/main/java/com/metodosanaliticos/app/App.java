5a8804560fa7dc3d70907355916d8b87c7dd46a6
Gabriel Teiga <gabrielteiga99@gmail.com>
2025-09-07 17:54:13 -0300

    feat: random number generator

--- src/main/java/com/metodosanaliticos/app/App.java
+++ src/main/java/com/metodosanaliticos/app/App.java
@@ -1,13 +1,50 @@
 package com.metodosanaliticos.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class App {
+    private final static BigInteger c_x0 = new BigInteger("123456789");
+    private final static BigInteger c_a  = new BigInteger("1664525");
+    private final static BigInteger c_c  = new BigInteger("1013904223");
+    private final static BigInteger c_M  = new BigInteger(new BigInteger("2").pow(32).toString());
+
+    public static void main(String[] args) {
+        NumberGenerator numberGenerator = new NumberGenerator(
+                c_x0,
+                c_a ,
+                c_c ,
+                c_M
+            );
+
+        for (int i = 1000000; i > 0; i--)
+        {
+            double randomNumber = numberGenerator.nextRandom();
+            System.out.println(randomNumber);
+        }
+    }
+
+    public static class NumberGenerator
     {
-        System.out.println( "Hello World!" );
+        private BigInteger x0; // Seed
+        private BigInteger x;  // Last number generated
+        private BigInteger a;  // Multiplier
+        private BigInteger c;  // Increment
+        private BigInteger M;  // Module
+
+        private NumberGenerator(BigInteger x0, BigInteger a, BigInteger c, BigInteger M)
+        {
+            this.x0 = new BigInteger(x0.toString());
+            this.x  = this.x0;
+            this.a  = new BigInteger(a.toString());
+            this.c  = new BigInteger(c.toString());
+            this.M  = new BigInteger(M.toString());
+        }
+
+        public double nextRandom()
+        {
+            this.x = ((this.a.multiply(this.x)).add(this.c)).mod(this.M);
+            return new BigDecimal(this.x).divide(new BigDecimal(this.M)).doubleValue();
+        }
     }
 }
